c4e26c0759013dc15fe928024fcf9389fe9c8c12
Ivan Rakic <irakic@gmail.com>
2024-06-17 22:45:00 +0200

    Fixing env and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+# Log function for better readability and debugging
+log() {
+  echo "::debug::$1" >> $GITHUB_OUTPUT
+}
 
+# Ensure the RCLONE_CONF environment variable is set
+if [[ -n "$RCLONE_CONF" ]]; then
+  log "Setting up rclone config..." >> $GITHUB_OUTPUT
+  mkdir -p ~/.config/rclone
+  echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
+fi
 
 sh -c "rclone $*" 
 
