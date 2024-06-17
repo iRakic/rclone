84f885586280f4a25f5c23277fff8120066f7587
Ivan Rakic <irakic@gmail.com>
2024-06-17 23:08:53 +0200

    Add debug options

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+echo "Running rclone with arguments: $*"
+
 # Ensure the RCLONE_CONF environment variable is set
 if [[ -n "$RCLONE_CONF" ]]; then
   mkdir -p ~/.config/rclone
   echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 fi
 
-rclone "$@"
+OUTPUT=$(sh -c "rclone $*")
+echo "$OUTPUT"
