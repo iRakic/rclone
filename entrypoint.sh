f5a6f45e2b5073d8c238f97299b65a5a323461cd
iRakic <irakic@gmail.com>
2024-06-18 01:53:37 +0200

    Refactoring and remove extensive logging (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,12 @@
 
 set -e
 
-# Log function for better readability and debugging
-log() {
-  echo "::debug::$1"
-}
+echo "Running rclone with arguments: $*"
 
 # Ensure the RCLONE_CONF environment variable is set
 if [[ -n "$RCLONE_CONF" ]]; then
-  log "Setting up rclone config..."
   mkdir -p ~/.config/rclone
   echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 fi
 
-# Check if rclone command is provided
-if [[ -z "$*" ]]; then
-  echo "::error::No rclone command provided."
-  exit 1
-fi
-
-# Execute rclone command
-log "Executing rclone command..."
-rclone "$@"
-log "Rclone command finished successfully."
+rclone -v $*
