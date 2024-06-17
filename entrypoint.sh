9beabd972ee3bf7c66b06851f09e2bfd66ad1972
Ivan Rakic <irakic@gmail.com>
2024-06-17 22:55:20 +0200

    Removing debug message

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 
 # Ensure the RCLONE_CONF environment variable is set
 if [[ -n "$RCLONE_CONF" ]]; then
-  echo "Setting up rclone config..." >> "$GITHUB_OUTPUT"
   mkdir -p ~/.config/rclone
   echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 fi
