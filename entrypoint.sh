3539c6a02a0f3714f1ff1d39e3b2e4501ba4efde
Ivan Rakic <irakic@gmail.com>
2024-06-17 22:32:43 +0200

    Run only command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,7 @@
 
 set -e
 
-# Log function for better readability and debugging
-log() {
-  echo "::debug::$1" >> $GITHUB_OUTPUT
-}
 
-# Ensure the RCLONE_CONF environment variable is set
-if [[ -n "$RCLONE_CONF" ]]; then
-  log "Setting up rclone config..." >> $GITHUB_OUTPUT
-  mkdir -p ~/.config/rclone
-  echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
-fi
 
-output=$(sh -c "rclone $*" 2>&1  >> $GITHUB_OUTPUT) 
-exit_code=$?
+sh -c "rclone $*" 
 
-if [[ $exit_code -ne 0 ]]; then
-  echo "::error::Rclone command failed with exit code $exit_code." >> $GITHUB_OUTPUT
-  exit $exit_code
-fi
-
-log "Rclone command finished successfully." >> $GITHUB_OUTPUT
-
-exit 0
