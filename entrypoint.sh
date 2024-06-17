84d39d0b0f754382253e877469ad99613fdfa6a8
Ivan Rakic <irakic@gmail.com>
2024-06-17 22:51:25 +0200

    Fixing github output

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "### Starting RClone :rocket:" >> $GITHUB_STEP_SUMMARY
 
 # Ensure the RCLONE_CONF environment variable is set
 if [[ -n "$RCLONE_CONF" ]]; then
-  echo "Setting up rclone config..." >> $GITHUB_OUTPUT
+  echo "Setting up rclone config..." >> "$GITHUB_OUTPUT"
   mkdir -p ~/.config/rclone
   echo "$RCLONE_CONF" > ~/.config/rclone/rclone.conf
 fi
